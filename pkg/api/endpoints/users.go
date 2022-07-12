d22ef62e0d28f3923755066db61f90c3f4514ac7
Johanna Ojeling <johanna@ojeling.net>
2022-07-12 20:40:55 +0200

    Improved logging of errors

--- pkg/api/endpoints/users.go
+++ pkg/api/endpoints/users.go
@@ -2,6 +2,7 @@ package endpoints
 
 import (
 	"errors"
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -35,6 +36,7 @@ func (handler *UsersHandler) CreateUser(ctx *gin.Context) {
 	var userRequest schemas.UserRequest
 	err := ctx.ShouldBindJSON(&userRequest)
 	if err != nil {
+		log.Printf("invalid request body: %v", err)
 		ctx.AbortWithStatusJSON(
 			http.StatusBadRequest,
 			models.NewErrorMessage("invalid request body"),
@@ -45,6 +47,7 @@ func (handler *UsersHandler) CreateUser(ctx *gin.Context) {
 	user := userRequestToUserModel(userRequest)
 	err = handler.UsersRepository.CreateUser(user)
 	if err != nil {
+		log.Printf("error creating user: %v", err)
 		ctx.AbortWithStatusJSON(
 			http.StatusInternalServerError,
 			models.NewErrorMessage("error creating user"),
@@ -60,6 +63,7 @@ func (handler *UsersHandler) GetUser(ctx *gin.Context) {
 	var userUri schemas.UserURI
 	err := ctx.BindUri(&userUri)
 	if err != nil {
+		log.Printf("invalid uri: %v", err)
 		ctx.AbortWithStatusJSON(
 			http.StatusBadRequest,
 			models.NewErrorMessage("invalid uri, expecting id"),
@@ -70,6 +74,7 @@ func (handler *UsersHandler) GetUser(ctx *gin.Context) {
 
 	user, err := handler.UsersRepository.GetUserById(id)
 	if errors.Is(err, repositories.ErrUserNotFound) {
+		log.Printf("user not found: %v", err)
 		ctx.AbortWithStatusJSON(
 			http.StatusNotFound,
 			models.NewErrorMessage("no user with id %q exists", id),
@@ -77,6 +82,7 @@ func (handler *UsersHandler) GetUser(ctx *gin.Context) {
 		return
 	}
 	if err != nil {
+		log.Printf("error getting user: %v", err)
 		ctx.AbortWithStatusJSON(
 			http.StatusInternalServerError,
 			models.NewErrorMessage("error retrieving user"),
@@ -89,8 +95,9 @@ func (handler *UsersHandler) GetUser(ctx *gin.Context) {
 }
 
 func (handler *UsersHandler) GetAllUsers(ctx *gin.Context) {
-	allUsers, err := handler.UsersRepository.GetAllUsers()
+	users, err := handler.UsersRepository.GetAllUsers()
 	if err != nil {
+		log.Printf("error getting users: %v", err)
 		ctx.AbortWithStatusJSON(
 			http.StatusInternalServerError,
 			models.NewErrorMessage("error retrieving users"),
@@ -98,8 +105,8 @@ func (handler *UsersHandler) GetAllUsers(ctx *gin.Context) {
 		return
 	}
 
-	userResponseList := make([]schemas.UserResponse, len(allUsers))
-	for i, user := range allUsers {
+	userResponseList := make([]schemas.UserResponse, len(users))
+	for i, user := range users {
 		userResponseList[i] = userModelToUserResponse(user)
 	}
 
@@ -110,6 +117,7 @@ func (handler *UsersHandler) UpdateUser(ctx *gin.Context) {
 	var userUri schemas.UserURI
 	err := ctx.BindUri(&userUri)
 	if err != nil {
+		log.Printf("invalid uri: %v", err)
 		ctx.AbortWithStatusJSON(
 			http.StatusBadRequest,
 			models.NewErrorMessage("invalid uri, expecting id"),
@@ -121,6 +129,7 @@ func (handler *UsersHandler) UpdateUser(ctx *gin.Context) {
 	var userRequest schemas.UserRequest
 	err = ctx.ShouldBindJSON(&userRequest)
 	if err != nil {
+		log.Printf("invalid request body: %v", err)
 		ctx.AbortWithStatusJSON(
 			http.StatusBadRequest,
 			models.NewErrorMessage("invalid request body"),
@@ -140,6 +149,7 @@ func (handler *UsersHandler) UpdateUser(ctx *gin.Context) {
 		}
 		err = handler.UsersRepository.CreateUser(newUser)
 		if err != nil {
+			log.Printf("error creating user: %v", err)
 			ctx.AbortWithStatusJSON(
 				http.StatusInternalServerError,
 				models.NewErrorMessage("error creating user"),
@@ -153,6 +163,7 @@ func (handler *UsersHandler) UpdateUser(ctx *gin.Context) {
 	}
 
 	if err != nil {
+		log.Printf("error updating user: %v", err)
 		ctx.AbortWithStatusJSON(
 			http.StatusInternalServerError,
 			models.NewErrorMessage("error updating user"),
@@ -168,6 +179,7 @@ func (handler *UsersHandler) DeleteUser(ctx *gin.Context) {
 	var userUri schemas.UserURI
 	err := ctx.BindUri(&userUri)
 	if err != nil {
+		log.Printf("invalid uri: %v", err)
 		ctx.AbortWithStatusJSON(
 			http.StatusBadRequest,
 			models.NewErrorMessage("invalid uri, expecting id"),
@@ -178,6 +190,7 @@ func (handler *UsersHandler) DeleteUser(ctx *gin.Context) {
 
 	err = handler.UsersRepository.DeleteUser(id)
 	if errors.Is(err, repositories.ErrUserNotFound) {
+		log.Printf("user not found: %v", err)
 		ctx.AbortWithStatusJSON(
 			http.StatusNotFound,
 			models.NewErrorMessage("no user with id %q exists", id),
@@ -185,6 +198,7 @@ func (handler *UsersHandler) DeleteUser(ctx *gin.Context) {
 		return
 	}
 	if err != nil {
+		log.Printf("error deleting user: %v", err)
 		ctx.AbortWithStatusJSON(
 			http.StatusInternalServerError,
 			models.NewErrorMessage("error deleting user"),
